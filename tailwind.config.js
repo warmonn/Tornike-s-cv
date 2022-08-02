a18b7be64c2d63669e3c715ef86d43e0cbea95b3
warmonn <84181927+warmonn@users.noreply.github.com>
2022-08-02 21:23:31 +0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        theme: "#2A2A2A",
+        primary: "#5F9EA0",
+      },
+      fontFamily: {
+        mont: ["Montserrat", "sans-serif"],
+      },
+    },
+    // ak chavamate 3xl xelit, ar mushaobs ise gasarkvevia
+    screens: {
+      "2xl": { max: "1835px" },
+      // => @media (max-width: 1535px) { ... }
+      "3xl": { max: "2835px" },
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+    },
+  },
+  plugins: [],
+};
